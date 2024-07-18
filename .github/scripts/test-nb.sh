84dab73247c55c93996435cb0a70ab667056b3f4
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-18 13:44:34 -0600

    update nb test dependencies

--- .github/scripts/test-nb.sh
+++ .github/scripts/test-nb.sh
@@ -3,6 +3,5 @@ source $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
 cd notebooks
 source install-deps
-ls
-pwd
+conda install -c ufs-community -c conda-forge --override-channels uwtools=2.3.2
 make test-nb
