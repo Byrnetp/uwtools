1a0c08cc0f94f8c1c96abe52a726577e9e9dc494
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-18 13:55:18 -0600

    local uwtools build attempt

--- .github/scripts/test-nb.sh
+++ .github/scripts/test-nb.sh
@@ -1,7 +1,7 @@
 set -ae
 source $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
+conda build recipe -c conda-forge --override-channels
 cd notebooks
 source install-deps
-conda install -c ufs-community -c conda-forge --override-channels uwtools=2.3.2
 make test-nb
