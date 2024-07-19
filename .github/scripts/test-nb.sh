ef08654b17916a63f62c37419266fd2bf402e159
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-19 16:35:55 -0600

    get latest uw version dynamically

--- .github/scripts/test-nb.sh
+++ .github/scripts/test-nb.sh
@@ -1,7 +1,9 @@
 set -ae
+
+LATEST_UWTOOLS_VERSION= conda search -c ufs-community --override-channels uwtools | tail -n 1 |  grep -Po '[0-9]+\.[0-9]+\.[0-9]'
+
 source $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
-LATEST_UWTOOLS_VERSION=conda search -c ufs-community --override-channels uwtools | tail -n 1 |  grep -Po '[0-9]+\.[0-9]+\.[0-9]'
 conda install -c ufs-community -c conda-forge --override-channels --repodata-fn repodata.json uwtools=$LATEST_UWTOOLS_VERSION
 cd notebooks
 source install-deps
