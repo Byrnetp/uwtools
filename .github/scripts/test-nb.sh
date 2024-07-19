dd9ba3b7e815eab7eff3f76f271d5fd333c91233
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-19 16:52:07 -0600

    test with full workflow

--- .github/scripts/test-nb.sh
+++ .github/scripts/test-nb.sh
@@ -1,6 +1,6 @@
 set -ae
 
-LATEST_UWTOOLS_VERSION= conda search -c ufs-community --override-channels uwtools | tail -n 1 |  grep -Po '[0-9]+\.[0-9]+\.[0-9]'
+LATEST_UWTOOLS_VERSION= conda search -c ufs-community --override-channels uwtools | tail -n 1 | grep -Po '[0-9]+\.[0-9]+\.[0-9]'
 
 source $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
