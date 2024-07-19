b9ec7e7d4318d398552eddba253e804efa26bed9
Travis Byrne <travis.p.byrne@gmail.com>
2024-07-19 11:43:11 -0600

    update test-nb.sh

--- .github/scripts/test-nb.sh
+++ .github/scripts/test-nb.sh
@@ -1,7 +1,7 @@
 set -ae
 source $(dirname ${BASH_SOURCE[0]})/common.sh
 ci_conda_activate
-conda install -c ufs-community -c conda-forge --override-channels uwtools
+conda install -c ufs-community -c conda-forge --override-channels uwtools=latest
 cd notebooks
 source install-deps
 make test-nb
